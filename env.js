535be5673df04b2ea0c5edc223ac726337cb29db
yunmiao <miao@MiaodeMacBook-Pro.local>
2018-02-07 16:55:05 +0800

    commit medusa config

--- env.js
+++ env.js
@@ -1,7 +1,13 @@
 var clusters = [
   {
+    NAME: "prod",
+    SCHEMA_REGISTRY: "http://g1-bdp-hdp-19.dns.guazi.com:8081", // https://schema-registry.demo.landoop.com
+    COLOR: "#141414" // optional
+  },
+
+  {
     NAME: "dev",
-    SCHEMA_REGISTRY: "http://localhost:8080",
+    SCHEMA_REGISTRY: "http://g1-bdp-hdp-19.dns.guazi.com:8081",
     COLOR: "red", // optional
     allowGlobalConfigChanges: true, // optional
     allowSchemaDeletion: true  // Supported for Schema Registry version >= 3.3.0 
